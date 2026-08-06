d897b1602df8d7b5a9d0628a88208718b9594d01
Antony Cervantes <antonny22c.c@gmail.com>
2026-08-06 15:35:04 -0600

    Trash

--- MicroServicios/MicroServicioUsuario/obj/Debug/net10.0/MicroServicioUsuario.AssemblyInfo.cs
+++ MicroServicios/MicroServicioUsuario/obj/Debug/net10.0/MicroServicioUsuario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroServicioUsuario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe2ca3d9e047babe31c07523c51c3442b43eb06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd74bd06c7c17f89eb4cc1a2ebe83371061d41c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroServicioUsuario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroServicioUsuario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
